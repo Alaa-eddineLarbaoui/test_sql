96a5af822bfc3f163a9d379d9d01e81602e4186e
ALAA <alaaeddinelarbaoui1@gmail.com>
2024-06-10 18:50:49 +0100

    j'ai creer les tables restans

--- fileSQL.sql
+++ fileSQL.sql
@@ -5,3 +5,35 @@ CREATE TABLE Invites (
     nom VARCHAR(255),
     statut VARCHAR(50)
 );
+
+CREATE TABLE Rooms (
+    id_room INT PRIMARY KEY,
+    nom VARCHAR(255)
+);
+
+
+CREATE TABLE Objets (
+    id_objet INT PRIMARY KEY,
+    nom_objet VARCHAR(255),
+    id_room INT,
+    FOREIGN KEY (id_room) REFERENCES Rooms(id_room)
+);
+
+
+CREATE TABLE Mouvements (
+    id INT PRIMARY KEY,
+    id_invite INT,
+    id_room INT,
+    temps date,
+    FOREIGN KEY (id_invite) REFERENCES Invites(id_invite),
+    FOREIGN KEY (id_room) REFERENCES Rooms(id_room)
+);
+
+
+CREATE TABLE Indices (
+    id_indice INT PRIMARY KEY,
+    description_Indice TEXT,
+    id_room INT,
+    FOREIGN KEY (id_room) REFERENCES Rooms(id_room)
+);
+
